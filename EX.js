ae3572f726a1eeb07a62f1a5302d714d8f69037c
Issam-Addi <essam.h.addi@gmail.com>
2023-03-15 17:36:08 +0300

    extra

---
+++ EX.js
@@ -0,0 +1,67 @@
+//// Q1: Evaluate each of the following Javascript expressions and show the value
+// console.log( -9 * 3);
+// console.log( "value " + 50);
+// console.log( 17%5);
+// console.log( 5%17);
+// console.log( 5/10);
+// console.log( 4==4);
+// console.log( 4!=5);
+// console.log( 7<=8);
+// console.log(Math.ceil(x)- Math.floor(x));
+
+//// Q2 : Read a number (using prompt) and display it using alert
+// let Num1 = prompt("Enter number :");
+// alert(Num1);
+
+
+//// Q3 : Read in two numbers and display them in ascending order.
+// let Num1 =parseInt(prompt("Enter number1 :"));
+// let Num2 =parseInt(prompt("Enter number2 :"));
+// if (Num1 > Num2){
+//     console.log(Num2 + " " + Num1)
+// }
+//     else {
+//     console.log(Num1 + " " + Num2)
+// }
+
+///// Q4: Read in two numbers and display the larger
+// let Num1 =parseInt(prompt("Enter number1 :"));
+// let Num2 =parseInt(prompt("Enter number2 :"));
+// if (Num1 > Num2){
+//     console.log(Num1 + ": is larger !")
+// }
+//     else {
+//     console.log(Num2 + ": is larger !")
+// }
+
+//// Q5: Read two numbers and display their sum.  What problem did you encounter?
+// let Num1 = parseInt(prompt("Enter number1 :"));
+// let Num2 = parseInt(prompt("Enter number2 :"));
+// let sum = Num1 + Num2;
+// console.log("The sumation is: " + sum);
+
+
+//// Q6: If you have a “num” variable,  Your task is to print:
+
+// let num =parseInt(prompt("Enter number1 :"));
+// if (num == 1){
+//     console.log("ONE")
+// } else if (num == 2){
+//     console.log("TWO")
+// } else if (num == 3){
+//     console.log("THREE")
+// } else if (num == 4){
+//     console.log("FOUR")
+// } else if (num == 5){
+//     console.log("FIVE")
+// } else if (num == 6){
+//     console.log("SIX")
+// } else if (num == 7){
+//     console.log("SEVEN")
+// } else if (num == 8){
+//     console.log("EIGHT")
+// } else if (num == 9){
+//     console.log("NINE")
+// } else {
+//     console.log("PLEASE TRY AGAIN")
+// }
